b9dbf40450a6371ae9a4f2c8e7e555b0ea073f1e
Harbingerofme <harbingerofme@gmail.com>
2020-04-27 10:43:32 +0200

    Update setup script

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 git clone https://github.com/harbingerofme/RoR2Libs.git
+cd RoR2Libs
 git checkout additionals
-rmdir /S /Q RoR2Libs\.git
+cd ..
 copy RoR2Libs\*.dll .\
 copy RoR2Libs\*.xml .\
 rmdir /S /Q RoR2Libs
